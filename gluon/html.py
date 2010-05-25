0df6cd5583b3583f9dc7ce048362998f66ff332c
mdipierro <mdipierro@massimo-di-pierros-macbook.local>
2010-05-25 10:04:51 -0500

    fixed bug

--- gluon/html.py
+++ gluon/html.py
@@ -1641,9 +1641,9 @@ class web2pyHTMLParser(HTMLParser):
         self.parent.append(data)
     def handle_charref(self,name):
         if name[1].lower()=='x':
-            self.parent.append(unichr(int(ent[2:], 16)))
+            self.parent.append(unichr(int(name[2:], 16)))
         else:
-            self.parent.append(unichr(int(ent[1:], 10)))
+            self.parent.append(unichr(int(name[1:], 10)))
         pass
     def handle_entityref(self,name):
         self.parent.append(unichr(name2codepoint[name]))
