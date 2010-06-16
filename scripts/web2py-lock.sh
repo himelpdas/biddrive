84fc1a8a095eb725632cdefe9db838d66fd265b9
mdipierro <mdipierro@massimo-di-pierros-macbook.local>
2010-06-16 01:32:05 -0500

    lock with nobody access, thanks Yarko

--- scripts/web2py-lock.sh
+++ scripts/web2py-lock.sh
@@ -1,11 +1,11 @@
-chown -R root:root *.py
-chown -R root:root gluon
-chown -R root:root scripts
-chown -R root:root applications/*/modules/
-chown -R root:root applications/*/models/
-chown -R root:root applications/*/controllers/
-chown -R root:root applications/*/views/
-chown -R root:root applications/*/static/
-chown -R root:root applications/*/cron/
+chown -R nobody:nobody *.py
+chown -R nobody:nobody gluon
+chown -R nobody:nobody scripts
+chown -R nobody:nobody applications/*/modules/
+chown -R nobody:nobody applications/*/models/
+chown -R nobody:nobody applications/*/controllers/
+chown -R nobody:nobody applications/*/views/
+chown -R nobody:nobody applications/*/static/
+chown -R nobody:nobody applications/*/cron/
 
 echo "unlock with chown -R www-data:www-data ./"
