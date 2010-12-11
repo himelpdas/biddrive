2bb590c00250b37ad86b2d6352edaf81151391cb
mdipierro <mdipierro@massimo-di-pierros-macbook-2.local>
2010-12-11 15:06:43 -0600

    welcome license

--- applications/welcome/controllers/appadmin.py
+++ applications/welcome/controllers/appadmin.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
 
 # ##########################################################
 # ## make sure administrator is on localhost
--- applications/welcome/models/menu.py
+++ applications/welcome/models/menu.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*- 
-
+# this file is released under public domain and you can use without limitations                 
 #########################################################################
 ## Customize your APP title, subtitle and menus here
 #########################################################################
