ca85d748ba5056e400ac4f05ee038e2fad7aa9f8
mdipierro <mdipierro@massimo-di-pierros-macbook-2.local>
2011-03-16 10:58:22 -0500

    faster auth (no session saved) if not auth event

--- gluon/storage.py
+++ gluon/storage.py
@@ -61,7 +61,11 @@ class Storage(dict):
             return None
 
     def __setattr__(self, key, value):
-        self[key] = value
+        if value == None:
+            if key in self:
+                del self[key]
+        else:
+            self[key] = value
 
     def __delattr__(self, key):
         if key in self:
