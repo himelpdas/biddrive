66daaaf0fa130457b0ca1b1caf9bf4db757b1c66
mdipierro <mdipierro@massimo-di-pierros-macbook-2.local>
2011-04-28 15:57:53 -0500

    fixed recaptcha?

--- gluon/tools.py
+++ gluon/tools.py
@@ -590,7 +590,8 @@ class Mail(object):
 
 class Recaptcha(DIV):
 
-    API_SSL_SERVER = 'https://api-secure.recaptcha.net'
+    API_SSL_SERVER = 'https://www.google.com/recaptcha/api'
+    # was 'https://api-secure.recaptcha.net'
     API_SERVER = 'http://api.recaptcha.net'
     VERIFY_SERVER = 'api-verify.recaptcha.net'
 
