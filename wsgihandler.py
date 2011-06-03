6314f7d7cc0c379df966f39528b62e84969c2603
mdipierro <mdipierro@massimo-di-pierros-macbook-2.local>
2011-06-03 09:07:45 -0500

    allow print with mod_wsgi, thanks Joseph Piron

--- wsgihandler.py
+++ wsgihandler.py
@@ -28,6 +28,8 @@ path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(path)
 sys.path = [path]+[p for p in sys.path if not p==path]
 
+sys.stdout=sys.stderr
+
 import gluon.main
 
 if LOGGING:
