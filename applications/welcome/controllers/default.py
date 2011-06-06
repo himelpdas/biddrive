dd979e6c83246e793fe11dd0e5211d9c76c0bd99
mdipierro <mdipierro@jspearsvista.cstcis.cti.depaul.edu>
2011-06-06 16:18:41 -0500

    fixed issue 291

--- applications/welcome/controllers/default.py
+++ applications/welcome/controllers/default.py
@@ -61,7 +61,7 @@ def data():
     http://..../[app]/default/data/delete/[table]/[id[
     http://..../[app]/default/data/select/[table]
     http://..../[app]/default/data/search/[table]
-    but URls bust be signed, i.e. linked with
+    but URLs bust be signed, i.e. linked with
       A('table',_href=URL('data/tables',user_signature=True))
     or with the signed load operator
       LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
