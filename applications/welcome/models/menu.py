5cead4fc02f5442e5b43d0448dc6f98d3d20b251
mdipierro <mdipierro@host-001.homenet.telecomitalia.it>
2011-08-19 14:27:59 -0500

    less enterprisy, thanks Anthony

--- applications/welcome/models/menu.py
+++ applications/welcome/models/menu.py
@@ -9,9 +9,9 @@ response.subtitle = T('customize me!')
 
 #http://dev.w3.org/html5/markup/meta.name.html
 response.meta.author = 'you'
-response.meta.description = 'Free and open source full-stack enterprise framework for agile development of fast, scalable, secure and portable database-driven web-based applications. Written and programmable in Python'
+response.meta.description = 'Free and open source full-stack framework for agile development of fast, scalable, secure and portable database-driven web-based applications. Written and programmable in Python'
 response.meta.keywords = 'web2py, python, framework'
-response.meta.generator = 'Web2py Enterprise Framework'
+response.meta.generator = 'Web2py Web Framework'
 response.meta.copyright = 'Copyright 2007-2010'
 
 
