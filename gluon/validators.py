e2b85c4003795f4caf48dfaa1deb78a2e7a358aa
mdipierro <mdipierro@massimo-di-pierros-macbook-2.local>
2011-08-28 22:58:50 -0500

    str(value) in self.theset, thanks Carlos

--- gluon/validators.py
+++ gluon/validators.py
@@ -478,7 +478,7 @@ class IS_IN_DB(Validator):
             if not [x for x in values if not x in self.theset]:
                 return (values, None)
         elif self.theset:
-            if value in self.theset:
+            if str(value) in self.theset:
                 if self._and:
                     return self._and(value)
                 else:
