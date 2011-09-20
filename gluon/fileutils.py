b44a879f0bb4912456dd4e4e9193c3020ff9a7ec
mdipierro <mdipierro@massimo-di-pierros-macbook-2.local>
2011-09-20 08:25:00 -0500

    version has optional type

--- gluon/fileutils.py
+++ gluon/fileutils.py
@@ -44,11 +44,12 @@ __all__ = [
     ]
 
 def parse_version(version = "Version 1.99.0 (2011-09-19 08:23:26)"):
-    re_version = re.compile('[^\d]+ (\d+)\.(\d+)\.(\d+) \((.+?)\)')
+    re_version = re.compile('[^\d]+ (\d+)\.(\d+)\.(\d+)\s*(?P<type>[a-z]+)?\s*\((?P<datetime>.+?)\)')
     m = re_version.match(version)
     a,b,c = int(m.group(1)),int(m.group(2)),int(m.group(3)),
-    d = datetime.datetime.strptime(m.group(4),'%Y-%m-%d %H:%M:%S')
-    return (a,b,c,d)
+    s = m.group('type') or 'dev'
+    d = datetime.datetime.strptime(m.group('datetime'),'%Y-%m-%d %H:%M:%S')
+    return (a,b,c,s,d)
 
 def read_file(filename, mode='r'):
     "returns content from filename, making sure to close the file explicitly on exit."
