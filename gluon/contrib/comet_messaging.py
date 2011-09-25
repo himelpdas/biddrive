fe4739058bd4a1e21e8ace7c081d6b8b53f92e81
mdipierro <mdipierro@massimo-di-pierros-macbook-2.local>
2011-09-25 14:00:40 -0500

    http://code.google.com/p/web2py/issues/detail?id=442

--- gluon/contrib/comet_messaging.py
+++ gluon/contrib/comet_messaging.py
@@ -6,7 +6,7 @@ License: LGPLv3 (http://www.gnu.org/licenses/lgpl.html)
 
 Attention: Requires Chrome or Safari. For IE of Firefox you need https://github.com/gimite/web-socket-js
 
-1) install tornado
+1) install tornado (requires Tornado 2.1)
 
    easy_install tornado
 
