f87892f449791cbad92e6f85c02ebe1a960bdcea
Massimo Di Pierro <mdipierro@cs.depaul.edu>
2011-11-10 10:44:11 -0600

    cleancss

--- scripts/cleancss.py
+++ scripts/cleancss.py
@@ -11,10 +11,14 @@ try:
     data = datafile.read()
 finally:
     datafile.close()
-data = re.compile('\s*{\s*').sub(' { ', data)
-data = re.compile('\s*;\s*').sub('; ', data)
-data = re.compile('\s*}\s*').sub(' }\n', data)
+
 data = re.compile('[ ]+').sub(' ', data)
+data = re.compile('\s*{\s*').sub(' {\n    ', data)
+data = re.compile('\s*;\s*').sub(';\n    ', data)
+data = re.compile('\s*\*/\s*').sub('*/\n    ', data)
+data = re.compile('\s*}\s*').sub('\n}\n', data)
+data = re.compile('\n\s*\n').sub('\n', data)
+data = re.compile('[ ]+\n').sub('\n', data)
 
 print data
 
