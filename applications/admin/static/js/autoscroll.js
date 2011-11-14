1b3eca4bc0a2dbbdb9f4b210e49ba9c8ac27c2e8
Massimo Di Pierro <mdipierro@cs.depaul.edu>
2011-11-14 09:54:51 -0600

    http://code.google.com/p/web2py/issues/detail?id=509, thanks Dulakian

--- applications/admin/static/js/autoscroll.js
+++ applications/admin/static/js/autoscroll.js
@@ -1,47 +1,60 @@
-// autoscroll widget with jquery event(s)
+/*
+ * autoscroll.js widget with jquery event(s)
+ * Copyright 2011 Michael Whitford <michael@alwandassociates.com>
+ * License: BSD
+ */
 
-var toggleAutoScroll = function () {
-    // uses an obj attached to window for state
+// toggle - id of the toggle element for the click event
+// textarea  - id of the textarea to autoscroll
+var toggleAutoScroll = function (toggle, textarea) {
+    var autoScrollDebug = false;
+    var state = toggle + 'interval';
     var scrollOn = function() {
-        window.autoScroll = setInterval( function() {
-                // ui hack?  needs more testing
-                window.output.scrollTop = window.output.scrollHeight;
-        }, 3000);  // 3 second interval
+        if (autoScrollDebug) { console.log('on:', toggle, textarea); }
+        var onAnimation = function(name, target) {
+            jQuery('#' + name).unbind().click(function() {
+                toggleAutoScroll(name, target);
+            }).fadeTo(350, 1);
+        };
+        // uses obj attached to window for state
+        window[state] = {};
+        window[state].name = toggle;
+        window[state].target = textarea;
+        // get a handle to the textarea el
+        var area = jQuery('#' + textarea);
+        // set the interval
+        window[state].interval = setInterval(function() {
+            // ui hack?  needs more testing
+            area[0].scrollTop = area[0].scrollHeight;
+        }, 2500);  // 2.5 seconds
+        onAnimation(toggle, textarea);
     };
     var scrollOff = function() {
-        clearInterval(window.autoScroll);
-        delete window.autoScroll;
+        if (autoScrollDebug) { console.log('off:', toggle, textarea); }
+        var offAnimation = function(name, target) {
+            jQuery('#' + name).unbind().click(function() {
+                    toggleAutoScroll(name, target);
+                }).fadeTo(350, 0.55);
+        };
+        offAnimation(toggle, textarea);
+        clearInterval(window[state].interval);
+        window[state] = undefined;
     };
-    if (arguments.length > 0) {
-        // boolean test first arg force with true/false for on/off
-        if (arguments[0]) {
+    if (arguments.length == 2) {
+        if (typeof window[state] === 'undefined') {
             scrollOn();
         } else {
             scrollOff();
         }
-    } else if (typeof window.autoScroll == 'undefined') {
-        scrollOn();
-    } else {
-        scrollOff();
     }
+    return;
 };
 
-// jquery event(s)
+// jquery onready
 
 jQuery(document).ready(function () {
-    // force it on by default
-    toggleAutoScroll(true)
-    // click event
-    jQuery('#autoscroll').click(function() {
-        toggleAutoScroll();
-        if (typeof window.autoScroll == 'undefined') {
-            // 'off' state animation
-            jQuery(this).fadeTo(350, 0.55);
-        } else {
-            // 'on' state animation
-            jQuery(this).fadeTo(350, 1);
-        }
-    });
+    // turn it on by default
+    toggleAutoScroll('autoscroll', 'output');
 });
 
 // todo: some key - toggle off
