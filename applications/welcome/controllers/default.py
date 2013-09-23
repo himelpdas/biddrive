5765dda04f453ec917102f1747353ff6a792f9d2
Massimo Di Pierro <mdipierro@cs.depaul.edu>
2013-09-23 14:54:29 -0500

    fixed issue  1196:add language subfolders, thanks jamarcer

--- applications/welcome/controllers/default.py
+++ applications/welcome/controllers/default.py
@@ -16,8 +16,8 @@ def index():
     rendered by views/default/index.html or views/generic.html
 
     if you need a simple wiki simple replace the two lines below with:
-    """
     return auth.wiki()
+    """
     response.flash = T("Welcome to web2py!")
     return dict(message=T('Hello World'))
 
