75565fb4fd6ef164d98739a431335a0462b4ea8c
himel p das <himel.p.das@gmail.com>
2014-01-16 03:43:26 -0500

    added disk to cach middleware

--- applications/init/models/models_edmunds.py
+++ applications/init/models/models_edmunds.py
@@ -5,9 +5,18 @@ from edmunds import Edmunds
 ed = Edmunds(EDMUNDS_KEY)
 
 def ed_cache(URI, function, time_expire=60*60*24):
+	#will call ram >> disk >> API
+	def disk():
+		response = cache.disk(
+			URI,
+			function,
+			time_expire*7,
+		)
+		return response
+	#
 	response = cache.ram(
 		URI, #to make sure the same URI doesn't get called twice
-		function,
+		disk,
 		time_expire,
 	)
 	return response
