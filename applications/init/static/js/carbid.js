e75ace468c1600542c6ac5f7c71ef96ffc559fd6
vexity <vexity@gmail.com>
2014-03-05 18:58:42 -0500

    tmp commit

--- applications/init/static/js/carbid.js
+++ applications/init/static/js/carbid.js
@@ -1,12 +1,5 @@
 $(function () {
-    $(window).scroll(function() {
-        if ($(".navbar").offset().top>30) {
-            $(".navbar-fixed-top").addClass("sticky");
-        }
-        else {
-            $(".navbar-fixed-top").removeClass("sticky");
-        }
-    });
+
 
     // Flex
     if ($(".flexslider").length) {
