6c0622ed19145906ade0c8334fe28f1caca72bb7
Himel Das <himel.p.das@gmail.com>
2014-04-24 03:57:19 -0400

    test

--- applications/init/controllers/voice.py
+++ applications/init/controllers/voice.py
@@ -2,7 +2,7 @@ response.view="generic.json"
 def index():
 	return(
 		dict(
-			tropo=[{"say":{"value":"Guess what?"}}]
+			tropo=[{"say":{"value":"Guess what? http://www.phono.com/audio/troporocks.mp3"}}]
 			)
 	)
 def result():
