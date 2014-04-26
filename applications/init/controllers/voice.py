6a2f7ccbb9be46440f71d1227aeae1606e026ba3
Himel Das <himel.p.das@gmail.com>
2014-04-26 00:40:19 -0400

    minor

--- applications/init/controllers/voice.py
+++ applications/init/controllers/voice.py
@@ -2,7 +2,7 @@ response.view="generic.json"
 def index():
 	return(
 		dict(
-			tropo=[{"say":{"value":"Welcome to Biddrive. BidDrive is the greatest ever. TrueCar can kiss my ass. Hello Neal, Paul, Zaki, Barret, Rob, Christy, and Himel."}}]
+			tropo=[{"say":{"value":"Welcome to the Biddrive dot com call center. Testing pronunciation. Hello Neal, Paul, Zaki, Barret, Rob, Christy, and Himel."}}]
 			)
 	)
 def result():
