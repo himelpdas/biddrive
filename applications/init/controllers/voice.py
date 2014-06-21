d85359052e92b290a0c5dea44c0ff098c2dcf520
Himel Das <himel.p.das@gmail.com>
2014-06-20 22:56:41 -0400

    test twilio 1

--- applications/init/controllers/voice.py
+++ applications/init/controllers/voice.py
@@ -16,9 +16,29 @@ def result():
 """
 def index():
 	resp = twiml.Response()
-	resp.say("Hello from Bid Drive dot com")
+	resp.say("Hello! Welcome to bid drive dot com.")
+    with resp.gather(numDigits=10, action="handle_key", method="POST") as g:
+        g.say("Please dial your 10 digit winner code now.")
 	return(
 		dict(
-			resp=str(resp) #By default web2py escapes string characters passed to view, use XML function to preserve escapable characters (ex < > & etc.)
+			resp=str(resp) #XML() MOVED TO VIEW #By default web2py escapes string characters passed to view, use XML function to preserve escapable characters (ex < > & etc.)
 		)
 	)
+
+def handle_key():
+    """Handle key press from a user."""
+ 
+    # Get the digit pressed by the user
+    digit_pressed = request.post_vars['Digits']
+    if digit_pressed:
+        resp = twiml.Response()
+        # Dial (310) 555-1212 - connect that number to the incoming caller.
+        #resp.dial("+13105551212")
+        # If the dial fails:
+        resp.say("You dialed %s. Goodbye."%digit_pressed)
+ 
+        return dict(resp = str(resp))
+ 
+    # If the caller pressed anything but 1, redirect them to the homepage.
+    else:
+        redirect(URL('init', 'voice', 'index.xml'))
