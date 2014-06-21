4156ecc69a21576528b93ac5ac97ef18c26aa01e
Himel Das <himel.p.das@gmail.com>
2014-06-21 03:49:53 -0400

    test twilio 11

--- applications/init/controllers/voice.py
+++ applications/init/controllers/voice.py
@@ -16,8 +16,9 @@ def result():
 """
 def index():
 	resp = twiml.Response()
-	resp.say("Hello! Welcome to the bid drive dot com auction verification automated hot line.")
-	with resp.gather(numDigits=10, action="handle_key.xml", method="POST") as g:
+	if not request.vars['force_gather']:
+		resp.say("Hello! Welcome to the bid drive dot com auction verification automated hot line.")
+	with resp.gather(numDigits=10, action="handle_key.xml", method="POST") as g: #with is like try finally that automatically closes the file via calling __exit__()
 		g.say("Please dial your 10 digit winner code now.")
 	return(
 		dict(
@@ -30,15 +31,27 @@ def handle_key():
 	
 	# Get the digit pressed by the user
 	digit_pressed = request.post_vars['Digits']
-	if digit_pressed:
+	if len(digit_pressed) = 10:
 		resp = twiml.Response()
 		# Dial (310) 555-1212 - connect that number to the incoming caller.
 		#resp.dial("+13105551212")
 		# If the dial fails:
-		resp.say("You dialed %s. Is this correct?"%(str(list(digit_pressed)).strip('[').strip(']'),)) #change to csv so operator can say each number separate
-
+		with resp.gather(numDigits=1, action="handle_key_check.xml/%s"%digit_pressed, method="POST") as g: #change to csv so operator can say each number separate
+			g.say("You dialed %s. If this is correct, press 1. Otherwise press 2 now."%(str(list(digit_pressed)).strip('[').strip(']'),))
+		
 		return dict(resp = str(resp))
  
 	# If the caller pressed anything but 1, redirect them to the homepage.
 	else:
-		redirect(URL('init', 'voice', 'index.xml'))
+		redirect(URL('init', 'voice', 'index.xml', vars=dict(force_gather=True)))
+		
+def handle_key_check():
+	digit_pressed = request.post_vars['Digits']
+	winner_code = request.vars(0)
+	if len(digit_pressed) == 1 and winner_code:
+		resp = twiml.Response()
+		resp.say("Thank you. We will now connect you to your winning dealer. Please hold.")
+		return dict(resp = str(resp))
+	# If the caller pressed 2 or some other key
+	else:
+		redirect(URL('init', 'voice', 'index.xml', vars=dict(force_gather=True)))
