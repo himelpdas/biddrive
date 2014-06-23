7216f6855f930b4befa2790931b29cbfdaf1a2ae
Himel Das <himel.p.das@gmail.com>
2014-06-23 16:01:47 -0400

    test twilio 31

--- applications/init/controllers/voice.py
+++ applications/init/controllers/voice.py
@@ -58,10 +58,26 @@ def handle_key_check():
 			resp.say("Thank you. I will now connect you to your winning dealer. Please hold.")
 			winning_offer = db(db.auction_request_offer.id == winner_code_exists.auction_request_offer).select().last()
 			winning_dealer = db(db.dealership_info.owner_id == winning_offer.owner_id).select().last()
+			auction_request = db(db.auction_request.id == winning_offer.auction_request).select().last()
+			auction_request_vehicle = dict(color = winning_offer.color, year = auction_request.year, make = auction_request.make, model = auction_request.model, trim = auction_request.trim_name)
 			winning_dealer_phone_number = "+"+''.join(winning_dealer.phone.split("-"))#http://goo.gl/JhE2V
-			resp.dial(winning_dealer_phone_number)
+			resp.dial(winning_dealer_phone_number, url = URL("screen_for_machine.xml", vars = auction_request_vehicle).split('/')[-1],) #convert init/voice/screen_for_machine.xml?model=... into screen_for_machine.xml?model=...
+			resp.say("The call failed, or the remote party hung up. Goodbye.")
 			return dict(resp = str(resp))
 		else:
 			redirect(URL('init', 'voice', 'index.xml', vars=dict(message="I'm sorry. That code doesn't exist in our database.")))
 	# If the caller pressed 2 or some other key
 	redirect(URL('init', 'voice', 'index.xml', vars=dict(skip_message=True)))
+
+def screen_for_machine():
+	resp = twiml.Response()
+	message = "Hello. This is the bid drive dot com automatic validation service. You are the winning bidder for a {color} {year} {make} {model} {trim}. The buyer initiated this call and is on waiting on the line. Please press any key to connect to the buyer now. ".format(**request.vars)
+	with resp.gather(numDigits=1, action="screen_complete.xml", method="POST") as g:
+		g.say(message*3)
+	return dict(resp=str(resp))
+	
+def screen_complete():
+	resp = twiml.Response()
+	resp.say("Connecting.")
+	return dict(resp=str(resp))
+	
