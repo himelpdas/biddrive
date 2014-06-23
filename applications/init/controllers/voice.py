2d208ac178629a5e2c87fa9f67a28c813d403147
Himel Das <himel.p.das@gmail.com>
2014-06-22 23:07:27 -0400

    test 23

--- applications/init/controllers/voice.py
+++ applications/init/controllers/voice.py
@@ -22,7 +22,7 @@ def index():
 	gather = "Please %sdial your 10 digit winner code now."% 're' if bool(message or skip_message) else ''
 	if not skip_message:
 		resp.say(message or intro)
-	with resp.gather(numDigits=10, action="handle_key.xml", method="POST") as g: #with is like try finally that automatically closes the file via calling __exit__()
+	with resp.gather(numDigits=12, action="handle_key.xml", method="POST") as g: #with is like try finally that automatically closes the file via calling __exit__()
 		g.say(gather)
 	return(
 		dict(
@@ -35,7 +35,7 @@ def handle_key():
 	
 	# Get the digit pressed by the user
 	digit_pressed = request.post_vars['Digits']
-	if len(digit_pressed) == 10:
+	if len(digit_pressed) == 12:
 		resp = twiml.Response()
 		# Dial (310) 555-1212 - connect that number to the incoming caller.
 		#resp.dial("+13105551212")
@@ -46,8 +46,7 @@ def handle_key():
 		return dict(resp = str(resp))
  
 	# If the caller pressed anything but 1, redirect them to the homepage.
-	else:
-		redirect(URL('init', 'voice', 'index.xml', vars=dict(message="I'm sorry. It seems you incorrectly dialed your winner code.")))
+	redirect(URL('init', 'voice', 'index.xml', vars=dict(message="I'm sorry. It seems you incorrectly dialed your winner code.")))
 		
 def handle_key_check():
 	digit_pressed = request.post_vars['Digits']
@@ -61,5 +60,4 @@ def handle_key_check():
 		else:
 			redirect(URL('init', 'voice', 'index.xml', vars=dict(message="I'm sorry. That code doesn't exist in our database.")))
 	# If the caller pressed 2 or some other key
-	else:
-		redirect(URL('init', 'voice', 'index.xml', vars=dict(skip_message=True)))
+	redirect(URL('init', 'voice', 'index.xml', vars=dict(skip_message=True)))
