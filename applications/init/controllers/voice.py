2b507e64618c8c7e4fe3bc0ae158b004fcc8cbfe
Himel Das <himel.p.das@gmail.com>
2014-06-24 02:23:42 -0400

    test 53

--- applications/init/controllers/voice.py
+++ applications/init/controllers/voice.py
@@ -60,12 +60,12 @@ def handle_key_check():
 			winning_dealer = db(db.dealership_info.owner_id == winning_offer.owner_id).select().last()
 			#contact_made = winning_offer.update_record(contact_made=True)
 			auction_request = db(db.auction_request.id == winning_offer.auction_request).select().last()
-			color_names = dict(map(lambda id,name: [id,name], auction_request.color_preference, auction_request.color_names))
-			auction_request_vehicle = dict(color = color_names[winning_offer.color], year = auction_request.year, make = auction_request.make, model = auction_request.model, trim = auction_request.trim_name)
+			color_names = dict(map(lambda id,name: [id,name], auction_request.color_preference, auction_request.simple_color_names))
+			auction_request_vehicle = dict(color = color_names[winning_offer.color], year = auction_request.year, make = auction_request.make, model = auction_request.model, id=auction_request.id) #trim = auction_request.trim_name)
 			winning_dealer_phone_number = "+"+''.join(winning_dealer.phone.split("-"))#http://goo.gl/JhE2V
 			screen_for_machine_url = URL("screen_for_machine.xml", vars = auction_request_vehicle, scheme=True, host=True)#.split('/')[-1] #url MUST BE absolute, action can be absolute or relative!
 			dialer = resp.dial(callerId = TWILIO_NUMBER_CALLER_ID) #convert init/voice/screen_for_machine.xml?model=... into screen_for_machine.xml?model=...
-			dialer.append(twiml.Number(winning_dealer_phone_number, url = screen_for_machine_url, method="POST"))
+			dialer.append(twiml.Number(winning_dealer_phone_number, url = screen_for_machine_url, method="POST")) #allows for interaction (ie. gather) with dealer before he enters the call.. must hang up explicitly if unresponsive or call will connect
 			#TODO figure out a way to play music while ringing #dialer.append(twiml.Conference(winner_code, waitUrl=URL('static','audio/on_hold_music.mp3', scheme=True, host=True) ) ) #room name is first argument
 			resp.say("The call failed, or the remote party hung up. Goodbye.")
 			return dict(resp = str(resp))
@@ -77,13 +77,14 @@ def handle_key_check():
 
 def screen_for_machine():
 	resp = twiml.Response()
-	message = "Hello. This is the bid drive dot com automatic validation service. You are the winning bidder for a {color} {year} {make} {model} {trim}. The buyer initiated this call and is waiting on the line. Please press any key to connect to the buyer now. ".format(**request.vars)
-	with resp.gather(numDigits=1, action="screen_complete.xml", method="POST") as g:
+	message = "This is the bid drive dot com automatic validation system. Press any key to skip this message. Hello. You are the winning bidder of auction ID {id}, for a {color} {year} {make} {model}. The buyer initiated this call and is waiting on the line. Please press any key to connect to the buyer now. ".format(**request.vars)
+	with resp.gather(numDigits=1, action="screen_complete.xml", method="POST") as g: #if he pressed something go to a new function.
 		g.say(message*3)
-	resp.hangup()
+	resp.hangup() #hang up if no gather
 	return dict(resp=str(resp))
 	
 def screen_complete():
+	#no need to test gather as any key is good
 	resp = twiml.Response()
 	resp.say("Connecting.")
 	return dict(resp=str(resp))
