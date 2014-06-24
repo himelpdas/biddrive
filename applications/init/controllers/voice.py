e43b508c81be4181942d3ce7a11c906ae07e448f
Himel Das <himel.p.das@gmail.com>
2014-06-23 23:55:17 -0400

    test 44

--- applications/init/controllers/voice.py
+++ applications/init/controllers/voice.py
@@ -62,7 +62,7 @@ def handle_key_check():
 			auction_request = db(db.auction_request.id == winning_offer.auction_request).select().last()
 			auction_request_vehicle = dict(color = winning_offer.color, year = auction_request.year, make = auction_request.make, model = auction_request.model, trim = auction_request.trim_name)
 			winning_dealer_phone_number = "+"+''.join(winning_dealer.phone.split("-"))#http://goo.gl/JhE2V
-			screen_for_machine_url = URL("screen_for_machine.xml", vars = auction_request_vehicle).split('/')[-1]
+			screen_for_machine_url = URL("screen_for_machine.xml", vars = auction_request_vehicle, scheme=True, host=True)#.split('/')[-1]
 			dialer = resp.dial() #convert init/voice/screen_for_machine.xml?model=... into screen_for_machine.xml?model=...
 			dialer.append(twiml.Number(winning_dealer_phone_number, url = screen_for_machine_url, method="POST"))
 			resp.say("The call failed, or the remote party hung up. Goodbye.")
